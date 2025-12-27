6d0fdc922d76bfc6cd779f85c0c567cc6f585b92
putrielisabetradjaudju1-wq <putrielisabetradjaudju1@gmail.com>
2025-12-28 01:08:55 +0800

    header dgn banner cafe & tagline

--- js/home.js
+++ js/home.js
@@ -1,1 +1,13 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
 
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+  });
+}
+
+setInterval(() => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}, 3000);
