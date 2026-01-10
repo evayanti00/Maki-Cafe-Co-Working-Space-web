5382c1de5e56a5908563ad360cce46a0f6a04163
evayanti00 <evayantimade9@gmail.com>
2026-01-10 14:49:27 +0800

    mengenrate gambar menggunakan array

--- js/promo_event.js
+++ js/promo_event.js
@@ -1,5 +1,28 @@
 console.log("Script sudah jalan");
 
+// bagian generate gambar otomatis
+const images = [
+  { src: "assets/img/promo1.png", alt: "promo"},
+  { src: "assets/img/promo2.png", alt: "promo"},
+  { src: "assets/img/event.png", alt: "event"},
+];
+
+const promoSection = document.getElementById("promoEvent");
+
+images.forEach(img => {
+  const div = document.createElement("div");
+  div.classList.add("img-container");
+
+  const images = document.createElement("img");
+  images.src = img.src;
+  images.alt = img.alt;
+  images.classList.add("clickable");
+
+  div.appendChild(images);
+  promoSection.appendChild(div);
+});
+
+// bagian modal
 const modal = document.getElementById("modal");
 const modalimg = document.getElementById("modal-img");
 const captionText = document.getElementById("caption");
@@ -14,13 +37,14 @@ document.querySelectorAll(".clickable").forEach(img => {
   });
 });
 
+//Tombol close
 closeBtn.onclick = () => {
   modal.style.display = "none";
 };
 
-
-
-
-
-
-
+//Tambahan: klik di luar gambar untuk menutup modal
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
